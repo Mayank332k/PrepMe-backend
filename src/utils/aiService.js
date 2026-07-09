6467dec8f2de43a48de4747fa2c092d862cd18b4
Mayank Singh <singhmayank4146@gmail.com>
2026-07-09 22:17:27 +0530

    model upgrade

--- src/utils/aiService.js
+++ src/utils/aiService.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { getSummarizerPrompt, getResumeParsingPrompt } = require('./prompts');
 
 const NVIDIA_API_KEY = process.env.NVIDIA_API_KEY;
-const NVIDIA_MODEL = "meta/llama-3.1-8b-instruct";
+const NVIDIA_MODEL = "meta/llama-3.2-11b-vision-instruct";
 
 const nvidiaApi = axios.create({
   baseURL: 'https://integrate.api.nvidia.com/v1',
